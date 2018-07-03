c19fc95d38b7007a833eb72d5eb83ec0c3ee6944
visu27 <viswanathsrikanthbathina@gmail.com>
2018-07-03 14:37:48 +0530

    Add files via upload
    improved accuracy by changing lambda and max_iterations and learning rate

--- nn.py
+++ nn.py
@@ -1,41 +1,51 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
-from sklearn import preprocessing,metrics
+from sklearn import metrics
 import itertools
 import collections
 from statistics import mean,stdev
-
 import matplotlib.pyplot as plt
 f = pd.read_csv('FTCT-Chem-YS-UTS 4.csv')
 T1 = f.iloc[:, 0:2]
+for j in range(0,2):
+    for i in range(3,13):
+        X=f.iloc[:,i]
+        plt.figure()
+        plt.xlabel('input %(i)d'%{'i':i-2})
+        plt.ylabel('output %(j)d'%{'j':j+1})
+        plt.scatter(X, T1.iloc[:,j],s=5)
+        plt.show()
 X=f.iloc[:,3:13]
-'''
-X=preprocessing.scale(X)
-T1=preprocessing.scale(T1)
-'''
+xm=[]
+xs=[]
 for i in range(0,10):
-    X.iloc[:,i]=X.iloc[:,i]-mean(X.iloc[:,i])
-    X.iloc[:,i]=X.iloc[:,i]/stdev(X.iloc[:,i])
-m=[]
-s=[]
+    xm.append(mean(X.iloc[:, i]))
+    xs.append(stdev(X.iloc[:, i]))
+    X.iloc[:,i]=X.iloc[:,i]-xm[i]
+    X.iloc[:,i]=X.iloc[:,i]/xs[i]
+tm=[]
+ts=[]
 for i in range(0,2):
-    m.append(mean(T1.iloc[:,i]))
-    s.append(stdev(T1.iloc[:,i]))
-    T1.iloc[:,i]=T1.iloc[:,i]-m[i]
-    T1.iloc[:,i]=T1.iloc[:,i]/s[i]
-X_train,X_test,T1_train,T1_test=train_test_split(X,T1,test_size=0.2,random_state=0)
+    tm.append(mean(T1.iloc[:,i]))
+    ts.append(stdev(T1.iloc[:,i]))
+    T1.iloc[:,i]=T1.iloc[:,i]-tm[i]
+    T1.iloc[:,i]=T1.iloc[:,i]/ts[i]
+X_train,X_test,T1_train,T1_test=train_test_split(X,T1,test_size=0.2,random_state=8)
 class layer():
     def __init__(self,n_in,n_out):
+        np.random.seed(11)
         self.W=np.random.randn(n_in,n_out)
         self.b=np.zeros(n_out)
     def get_pi(self):
         return itertools.chain(np.nditer(self.W),np.nditer(self.b))
     def get_o(self,X):
         return X.dot(self.W)+self.b
-    def get_pg(self,X,og,b):##very doubtful
+    def get_pg(self,X,og,b):
+        lda=0.045
         JW=X.T.dot(og)
         JW/=b
+        JW += lda * self.W
         Jb=np.sum(og,axis=0)
         Jb/=b
         return [g for g in itertools.chain(np.nditer(JW),np.nditer(Jb))]
@@ -55,7 +65,7 @@ class layer():
             else:
                 self.b[i-r*c]=ab
             i+=1
-h1=6
+h1=7
 h2=4
 layers=[]
 layers.append(layer(X_train.shape[1],h1))
@@ -69,7 +79,7 @@ def forward(input,layers):
         a.append(y)
         X=a[-1]
     return a
-def backward(a,t,layers,b):##doubt
+def backward(a,t,layers,b):
     pg=collections.deque()
     og=None
     for l in reversed(layers):
@@ -80,19 +90,14 @@ def backward(a,t,layers,b):##doubt
         else :
             ig=l.get_ig(og)
         X=a[-1]
-        g=l.get_pg(X,og,b)##very doubtful
+        g=l.get_pg(X,og,b)
         pg.appendleft(g)
         og=ig
     return pg
 b=T1_train.shape[0]
-nb=X_train.shape[0]/b
-XT=zip(
-    np.array_split(X_train, 1, axis=0),
-    np.array_split(T1_train, 1, axis=0))
-maxiter=800
-lr=0.01
+maxiter=920
+lr=0.0028
 for i in range(maxiter):
-    ##for X,T in XT:
     a=forward(X_train,layers)
     pg=backward(a,T1_train,layers,b)
     for l, lbg in zip(layers, pg):
@@ -101,16 +106,35 @@ yt=T1_test
 a=forward(X_test,layers)
 yp=a[-1]
 for i in range(0,2):
-    yp.iloc[:, i] = yp.iloc[:, i] * s[i] + m[i]
-    yt.iloc[:, i] = yt.iloc[:, i] * s[i] + m[i]
+    yp.iloc[:, i] = yp.iloc[:, i] * ts[i] + tm[i]
+    yt.iloc[:, i] = yt.iloc[:, i] * ts[i] + tm[i]
+    tl=[x for x in yp.iloc[:, i]]
     plt.figure()
     plt.xlabel('predicted output')
     plt.ylabel('actual output')
-    plt.axes().set_xlim(xmin=230,xmax=670)
-    plt.axes().set_ylim(ymin=230,ymax=670)
+    plt.axes().set_xlim(xmin=300,xmax=600)
+    plt.axes().set_ylim(ymin=300,ymax=600)
     plt.scatter(yp.iloc[:, i], yt.iloc[:, i], s=5,)
+    plt.plot(yp.iloc[:, i],tl,)
     plt.show()
-print(yt)
-print(yp)
 acc=metrics.r2_score(yt,yp)
-print(acc)
+acc*=100
+print("the percentage accuracy of the model is %(acc)f"%{"acc":acc})
+'''
+yt=T1_train
+a=forward(X_train,layers)
+yp=a[-1]
+acc=metrics.r2_score(yt,yp)
+acc*=100
+print("the percentage accuracy of the model on training data is is %(acc)f"%{"acc":acc})
+'''
+s1=np.array([[881.875,845.625,78.512,0,653.144,90.535,0.045,0.384,1.32,0.047]])
+for i in range(0,10):
+    s1[0][i]=s1[0][i]-xm[i]
+    s1[0][i]=s1[0][i]/xs[i]
+a1=forward(s1,layers)
+predict1=a1[-1]
+predict1[0][0]= predict1[0][0] * ts[0] + tm[0]
+predict1[0][1]= predict1[0][1] * ts[1] + tm[1]
+print("yield of the given sample is ")
+print(predict1)
